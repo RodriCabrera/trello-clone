5b496fad421df7f1d4972d4cc5ac936458a228a3
Rodrigo <rodrigo1cabrera2@gmail.com>
2021-12-14 15:10:26 -0300

    Persist redux state in local storage

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
 import columnsReducer from "../slices/columnsSlice";
 import cardsReducer from "../slices/cardsSlice";
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+	columns: columnsReducer,
+	cards: cardsReducer,
+});
+const persistConfig = {
+	key: "root",
+	storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-	reducer: { columns: columnsReducer, cards: cardsReducer },
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 });
+
+export const persistor = persistStore(store);
